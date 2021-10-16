c5dbf0ad4239a5cee71bfe098b65a489ef819863
MoacirMSJ <moacirmascarenha86@gmail.com>
2021-10-16 18:30:54 -0300

    Adicionada conexão o redis.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,30 @@
-import { Controller, Get } from '@nestjs/common';
+import { CACHE_MANAGER, Controller, Get, Inject, Param } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Cache } from 'cache-manager';
 
-@Controller()
+@Controller('/monitoring')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  fakeString = 'my name is naveen';
+  constructor(
+    private readonly appService: AppService,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
+  ) {}
 
+  @Get(':id')
+  async setGetSimpleString(@Param('id') id: string) {
+    const value = await this.cacheManager.get(id);
+    if (value) {
+      console.log('Encontrou');
+      return {
+        data: value,
+      };
+    }
+    await this.cacheManager.set(id, '{joao:"Joazin"}', { ttl: 300 });
+    return {
+      data: this.fakeString,
+      loadsFrom: 'fake database',
+    };
+  }
   @Get()
   getHello(): string {
     return this.appService.getHello();
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
-import { Module } from '@nestjs/common';
+import { Module, CacheModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import * as redisStore from 'cache-manager-redis-store';
 
 @Module({
   imports: [
+    CacheModule.register({
+      store: redisStore,
+      host: 'localhost',
+      port: 6379,
+      db: '0',
+    }),
     ConfigModule.forRoot({
       envFilePath: '.env',
     }),
