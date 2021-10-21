ff3d5a36ebb6208fa451e5327bcd2f27ad30d285
MoacirMSJ <moacirmascarenha86@gmail.com>
2021-10-20 22:32:47 -0300

    Conflito

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module, CacheModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-<<<<<<< HEAD
 import * as redisStore from 'cache-manager-redis-store';
 import { HttpModule } from '@nestjs/axios';
 
@@ -18,12 +17,6 @@ import { HttpModule } from '@nestjs/axios';
     ConfigModule.forRoot({
       envFilePath: '.env',
     }),
-=======
-
-@Module({
-  imports: [
-    ConfigModule.forRoot({ envFilePath: '.env' })
->>>>>>> 43c7c6816c82f793ac6e6bd50c44e95cabb8d1ae
   ],
   controllers: [AppController],
   providers: [AppService],
